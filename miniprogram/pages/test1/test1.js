5a85ce834eb600922d0ba10c810f573338dd095a
周子龙 <zhouzilong@zhouzilongdeMacBook-Pro.local>
2020-07-28 15:57:54 +0800

    submit completed

--- miniprogram/pages/test1/test1.js
+++ miniprogram/pages/test1/test1.js
@@ -1,9 +1,13 @@
 const app = getApp()
+const db = wx.cloud.database()
 wx.cloud.init({
   env:'test-0xsoi',
 })
 Page({
   data: {
+    //   登录相关信息
+    logged:false,
+    openid:'',
     //   错误提示
      error:'',
     //   输入控制
@@ -40,6 +44,7 @@ Page({
                 longitude:'',
                 mobile:'',
                 imageCnt:0,
+                extraInfo:'',
                 urls:[],},
     //   选项框
       radioItems:[
@@ -51,7 +56,31 @@ Page({
       ],
     //   照片上传
       files: [],
+      tempFilePaths:[],
   },
+
+  //初次渲染完成进行登录，获得用户相关信息
+  onReady: function () {
+    wx.cloud.callFunction({
+      name : 'login',
+      data : {}
+    }).then((res) =>{
+    //   console.log(res);
+      db.collection('users').where({
+        _openid : res.result.openid
+      }).get().then((res) =>{
+        if( res.data.length ){
+          app.userInfo = Object.assign( app.userInfo, res.data[0]);
+          this.setData({
+            nickName : app.userInfo.nickName,
+            logged : true,
+            openid : app.userInfo._openid,
+          });
+        }
+      });
+    });
+  },
+
   onLoad() {
       this.setData({
           selectFile: this.selectFile.bind(this),
@@ -59,8 +88,9 @@ Page({
       })
   },
 
-//   提交表单
+//   提交表单，此时应该是有了用户的相关信息的！
     submitForm: function(e) {
+        var that = this;
         this.selectComponent('#form').validate((valid, errors) => {
             console.log('valid', valid, errors)
             if (!valid) {
@@ -72,36 +102,56 @@ Page({
                     })
                 }
             } else {
+                wx.showLoading({
+                    title: '正在提交',
+                })
+                // 时间戳
+                var timestamp = Date.parse(new Date()); 
+                //提交照片
+                var tempFilePaths = that.data.tempFilePaths;
+                var cloudPaths = [];
+                console.log("outside upload", tempFilePaths)
+                for (let i = 0; i < tempFilePaths.length; i++){
+                    var filePath = tempFilePaths[i];
+                    var cloudPath = './' + that.data.openid + '/'  + timestamp + '_' + i + filePath.match(/\.[^.]+?$/)[0];
+                    console.log("in upload", cloudPath)
+                    wx.cloud.uploadFile({
+                        cloudPath: cloudPath, //云
+                        filePath: filePath,   //本地
+                        success: (res) =>{
+                            that.setData({
+                                ['formData.urls']: that.data.formData.urls.concat(cloudPath)
+                            })
+                        }
+                    })
+
+                }
+                // 照片全部提交成功 
+                console.log("on submmiting database", this.data.formData);
+                // 提交其他信息
+                db.collection("images").add({
+                    data:{
+                        openid: that.data.openid,
+                        formData: that.data.formData,
+                        timestamp : timestamp
+                    }
+                  })
                 wx.showToast({
                     title: "提交成功！"
+                })
+
+                wx.navigateTo({
+                    url: '../mes_success/mes_success',
+                    success: function(res) {
+                        var id = that.data.openid + timestamp;
+                        // 通过eventChannel向被打开页面传送数据
+                        res.eventChannel.emit('acceptID', { id: id })
+                    }
                 })
             }
         })
     },
-    // submitForm: function(e){
-    //     this.selectComponent('#form').validate((valid, errors) =>{
-    //         console.log('valid', valid, errors);
-    //         // 如果出错
-    //         if(errors.length){
-    //             for( let i = 0, len = errors.length; i < len; i++){
-    //                 if (errors[i].name == "type"){
-    //                     this.setData({
-    //                         typeError: errors[i].message
-    //                     });
-    //                 }
-    //                 else{
-    //                     this.setData({
-    //                         mobileError: errors[i].message
-    //                     });
-    //                 }
-    //             }
-    //             return ;
-    //         }
-    //         else{
-    //             // 上传表单。。。
-    //         }
-    //     })
-    // },
+
 //   输入手机号
     phoneInputHandle: function(e){
         this.setData({
@@ -133,6 +183,7 @@ Page({
     inputHandle: function(e){
         this.setData({
             inputLength: e.detail.cursor,
+            ['formData.extraInfo'] : e.detail.value
         })
     },
 
@@ -145,53 +196,51 @@ Page({
         }
         this.setData({
             radioItems: radioItems,
-            [`formData.type`]: e.detail.value  //存在这里！
+            ['formData.type']: e.detail.value  //存在这里！
         });
     },
 
 //   选择照片
-  chooseImage: function (e) {
-      var that = this;
-      wx.chooseImage({
-          sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-          sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-          success: function (res) {
-              // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-              that.setData({
-                  files: that.data.files.concat(res.tempFilePaths)
-              });
-          }
-      })
+    chooseImage: function (e) {
+        var that = this;
+        wx.chooseImage({
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                
+            }
+        })
   },
-  previewImage: function(e){
-      wx.previewImage({
-          current: e.currentTarget.id, // 当前显示图片的http链接
-          urls: this.data.files // 需要预览的图片http链接列表
-      })
-  },
-  selectFile(files) {
-      console.log('files', files)
-      // 返回false可以阻止某次文件上传
-  },
-  uplaodFile(files) {
-      console.log('upload files', files)
-      // 文件上传的函数，返回一个promise
-      return new Promise((resolve, reject) => {
-            console.log("data:", this.data);
+    previewImage: function(e){
+        wx.previewImage({
+            current: e.currentTarget.id, // 当前显示图片的http链接
+            urls: this.data.files // 需要预览的图片http链接列表
+        })
+    },
+    selectFile(files) {
+        // console.log('files', files)
+        // 返回false可以阻止某次文件上传
+    },
+    uplaodFile(files) {
+        //   console.log('upload files', files)
+        // 文件上传的函数，返回一个promise
+        return new Promise((resolve, reject) => {
+            // console.log("data:", this.data);
             for (let i = 0; i < files.tempFilePaths.length; i++){
                 this.setData({
                     files: this.data.files.concat({url:files.tempFilePaths[i], loading:false}),
-                    ['formData.urls'] : this.data.formData.urls.concat(files.tempFilePaths[i]),
-                    ['formData.imageCnt'] : this.data.formData.imageCnt+1,
+                    tempFilePaths: this.data.tempFilePaths.concat(files.tempFilePaths),
+                    ['formData.imageCnt'] : this.data.formData.imageCnt+1
                 })
             }
-
-      })
-  },
-  uploadError(e) {
-      console.log('upload error', e.detail)
-  },
-  uploadSuccess(e) {
-      console.log('upload success', e.detail)
-  }
+            // console.log("in upload image data:", this.data);
+        })
+    },
+    uploadError(e) {
+        console.log('upload error', e.detail)
+    },
+    uploadSuccess(e) {
+        console.log('upload success', e.detail)
+    }
 });
