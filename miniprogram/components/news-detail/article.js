65afb72086e516e9ed6bd3d8aa36df5bd082a99c
zhouzilong2020 <529620861@qq.com>
2021-05-25 08:36:38 +0800

    news-detail

--- miniprogram/components/news-detail/article.js
+++ miniprogram/components/news-detail/article.js
@@ -7,14 +7,30 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    news: Object
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    title: '“活力四射”-发起睦邻活动',
+    subtitle: '——上海市杨浦区延吉新村街道第二睦邻中心',
+    author: '一个发稿人',
+    createTime: '2020-03-08',
+    image: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3026939796,485761977&fm=26&gp=0.jpg',
+  },
+  lifetimes: {
+    ready: function () {
+      const that = this
+      this.setData({
+        title: that.data.news.title,
+        subtitle: that.data.news.subtitle,
+        author: that.data.news.author,
+        createTime: new Date(that.data.news.create_time).Format("yyyy-MM-dd"),
+        image: that.data.news.image,
+      })
+    }
   },
 
   /**
--- miniprogram/pages/news/news.js
+++ miniprogram/pages/news/news.js
@@ -5,13 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    news: {},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const that = this
+    const eventChannel = this.getOpenerEventChannel()
+    // 监听acceptDataFromOpenerPage事件，获取上一页面通过eventChannel传送到当前页面的数据
+    eventChannel.on('forwardNews', res => {
+      const news = res.data.news
+      that.setData({
+        news: news
+      })
+    })
 
   },
 
